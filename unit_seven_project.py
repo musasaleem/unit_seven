1004215ec82eccf6b32af29770d6d4ec93b72a94
Musa <musa@iMac10.local>
2018-11-01 10:13:34 -0400

    project almost done

--- unit_seven_project.py
+++ unit_seven_project.py
@@ -1,35 +1,60 @@
 def code():
     print("You will chose to decode encode or quit any code of your choosing")
-    option = int(input("decode, encode or quit?"))
-    if option in ["encode", "e"]:
+    option = input("decode, encode or quit?")
+    if option in ["encode", "e", "encoded"]:
         print("you chose to encode")
-        return True
+        return "e"
     if option in ["decode", "d"]:
-        return
+        print("you chose to decode")
+        return "d"
     else:
-        return False
+        return "q"
+
 
 def main():
 
-            number = int(input("choose any number between 0-24"))
-            alphabet = "abcdefghijklmnopqrstuvwxyz"
-            first = alphabet[number:]
-            last = alphabet[0: number]
-            final = first + last
-            phrase = input("choose any phrase you want to encode")
-            for x in phrase:
-                code = phrase.index(x)
-                end = final[code]
-                print(end)
+    if code() == "e":
+        number = int(input("choose any number between 0-24"))
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        first = alphabet[number:]
+        last = alphabet[0: number]
+        final = first + last
+        encoded = ""
+        phrase = input("choose any phrase you want to encode")
+        for x in phrase:
+            if x == " ":
+                encoded = encoded + " "
+            else:
+                bet = alphabet.index(x)
+                end = final[bet]
+                encoded = encoded + end
+        print(encoded)
+
+    elif code() == "d":
+        number = int(input("choose any number between 0-24"))
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        last = alphabet[number:]
+        first = alphabet[0: number]
+        final = first + last
+        decoded = ""
+        phrase = input("choose any phrase you want to encode")
+        for x in phrase:
+            if x == " ":
+                decoded = decoded + " "
+            else:
+                bet = alphabet.index(x)
+                end = final[bet]
+                decoded = decoded + end
+        print(decoded)
 
 
             #number = int(input("choose any number between 0-24"))
             #alphabet = "abcdefghijklmnopqrstuvwxyz"
-0            #first = alphabet[number + 1:]
+          #first = alphabet[number + 1:]
            # last = alphabet[0: number]
            # final =  last + first
            # print(final)
 
 
 main()  #while code():
-        #while True:0000
+        #while True:0000i
