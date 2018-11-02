43addcc8c1ce7b63d8a486b84e49aae39f2487d5
Musa <musa@iMac10.local>
2018-11-02 11:13:06 -0400

    Final draft of unit seven project

--- unit_seven_project.py
+++ unit_seven_project.py
@@ -1,10 +1,21 @@
+# Musa Saleem
+# Date: 11/2/18
+# Last Modified: 11/2/18
+# Comments: This project is about the computer using code to either encode or decode a function using caesar rotation
+
+
 def code():
+    """
+    This is the code function where it is asking the user what they want to do, and whatever the user chooses, the
+    computer puts them on a certain function
+    :return: e for encode, d for decode, q for quit
+    """
     print("You will chose to decode encode or quit any code of your choosing")
     option = input("decode, encode or quit?")
     if option in ["encode", "e", "encoded"]:
         print("you chose to encode")
         return "e"
-    if option in ["decode", "d"]:
+    elif option in ["decode", "d"]:
         print("you chose to decode")
         return "d"
     else:
@@ -12,49 +23,43 @@ def code():
 
 
 def main():
-
-    if code() == "e":
+    choice = code()
+    if choice == "e":
         number = int(input("choose any number between 0-24"))
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         first = alphabet[number:]
         last = alphabet[0: number]
-        final = first + last
-        encoded = ""
+        final = first + last  # changing the alphabet around to encode it
+        encoded = ""  # Introducing the encoded function
         phrase = input("choose any phrase you want to encode")
         for x in phrase:
             if x == " ":
-                encoded = encoded + " "
+                encoded = encoded + " "  # Saying if there is a space in the phrase, keep the space and add it in rather
+                # than look for it in the alphabet function
             else:
                 bet = alphabet.index(x)
                 end = final[bet]
-                encoded = encoded + end
+                encoded = encoded + end  # Corresponding the phrase with the alphabet
         print(encoded)
-
-    elif code() == "d":
+    elif choice == "d":
         number = int(input("choose any number between 0-24"))
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        last = alphabet[number:]
-        first = alphabet[0: number]
-        final = first + last
-        decoded = ""
-        phrase = input("choose any phrase you want to encode")
+        alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        first = alphabet[number:]
+        last = alphabet[0: number]
+        final = first + last  # Changing alphabet to decode it
+        decoded = ""  # Introducing decode function
+        phrase = input("choose any phrase you want to decode")
         for x in phrase:
             if x == " ":
-                decoded = decoded + " "
+                decoded = decoded + " "  # Saying if there is a space in the phrase, keep the space and add it in rather
+                # than look for it in the alphabet function
             else:
                 bet = alphabet.index(x)
                 end = final[bet]
-                decoded = decoded + end
+                decoded = decoded + end  # Corresponding the phrase with the alphabet
         print(decoded)
+    elif choice == "q":
+        print("have a nice day")  # Quit function
 
 
-            #number = int(input("choose any number between 0-24"))
-            #alphabet = "abcdefghijklmnopqrstuvwxyz"
-          #first = alphabet[number + 1:]
-           # last = alphabet[0: number]
-           # final =  last + first
-           # print(final)
-
-
-main()  #while code():
-        #while True:0000i
+main()
